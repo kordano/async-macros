5cdfa302216c012d4b9d29588d6d87893598fbb3
kordano <konrad.kuehne@rocketmail.com>
2015-04-29 10:06:13 +0200

    rename again and new cljs-checker

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
+                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
+  :cljsbuild {:builds
+              {:dev
+               {:compiler {:output-to "target/main.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}}})
